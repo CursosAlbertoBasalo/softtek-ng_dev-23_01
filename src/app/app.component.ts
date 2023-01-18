3d7ae0a8aa9cab3e37b66ae28006adb34fb8af2b
Alberto Basalo <albertobasalo@hotmail.com>
2023-01-18 16:06:59 +0100

    feat: template outside

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,33 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "lab-root",
-  template: `
-    <header>
-      <h1> Welcome to {{ title }}! </h1>
-      <p>
-        sumar {{ numberOne }} + {{ numberTwo }} =
-        {{ sumar(numberOne, numberTwo) }}
-      </p>
-    </header>
-    <router-outlet></router-outlet>
-    <footer>
-      <span>{{ title }} app is running!</span>
-      <span>
-        <a [href]="repoUrl" target="_blank"> Repository on gitHub </a>
-      </span>
-      <span> ©️ {{ year }} </span>
-    </footer>
-  `,
+  templateUrl: "./app.component.html",
   styles: [],
 })
 export class AppComponent {
   title = "Activity bookings";
   repoUrl = "https://github.com/classroomLabs/softtek-angular_intro-enero";
   year = new Date().getFullYear();
-  numberOne = 77;
-  numberTwo = 88;
-
-  sumar(uno: number, dos: number) {
-    return uno + dos;
-  }
 }

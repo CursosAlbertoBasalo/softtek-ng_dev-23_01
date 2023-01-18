9ba17cf9e7984ecb789c51731a5b7af3229f1616
Alberto Basalo <albertobasalo@hotmail.com>
2023-01-18 18:25:44 +0100

    refactor: simple tags 15.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from "@angular/core";
 @Component({
   selector: "lab-root",
   template: `
-    <lab-header></lab-header>
+    <lab-header/>
     <main>
-      <router-outlet></router-outlet>
-      <lab-activities></lab-activities>
+      <router-outlet/>
+      <lab-activities/>
     </main>
-    <lab-footer></lab-footer>
+    <lab-footer/>
   `,
   styles: [],
 })

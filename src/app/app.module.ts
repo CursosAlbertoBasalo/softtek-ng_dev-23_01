48f3a8ba1e0187a65a11fbcb1e83acd6bd6ad5f4
Alberto Basalo <albertobasalo@hotmail.com>
2023-01-18 17:35:02 +0100

    feat: generate activities component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ActivitiesComponent } from './activities.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ActivitiesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
